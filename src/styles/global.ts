d90936e41aaa45e99cab0b707bd43a5014663f38
tiagobarros01 <tbarros1337@hotmail.com>
2022-04-19 19:29:01 -0300

    config: Configures global styles;

---
+++ src/styles/global.ts
@@ -0,0 +1,47 @@
+import { css } from "@emotion/react";
+
+export const GlobalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #eee;
+
+    font-family: "Poppins", sans-serif;
+
+    overflow: hidden;
+  }
+
+  @media (max-width: 1366px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 1200px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 992px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    html {
+      font-size: 81.25%;
+    }
+  }
+
+  @media (max-width: 576px) {
+    html {
+      font-size: 75%;
+    }
+  }
+`;
